95fd197173ff9a12389f706d756c22f583d47c61
Nihar Ranjan Das <niharranjandasmu@gmail.com>
2022-06-23 18:21:57 +0600

    stop activity before logout feature added

--- app/Http/Responses/LogoutResponse.php
+++ app/Http/Responses/LogoutResponse.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Responses;
 
+use App\Http\Repositories\ActivityRepo;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
 use Laravel\Fortify\Contracts\LogoutResponse as ContractsLogoutResponse;
 
@@ -10,6 +12,11 @@ class LogoutResponse implements ContractsLogoutResponse
     public function toResponse($request)
     {
         Session::remove('local');
+        $activityRepo = new ActivityRepo();
+        $activity = $activityRepo->getRunningActivity(Auth::id());
+        if ($activity) {
+            $activityRepo->stopActivity($activity);
+        }
         return redirect()->intended(config('login'));
     }
 }
